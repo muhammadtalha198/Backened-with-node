93f9235f90671582908352ae8e92c15122042c4a
talha ali <talha.ali@invozone.com>
2025-09-08 10:21:23 +0500

    connect to mongo atlas

--- ReadMe.md
+++ ReadMe.md
@@ -1,2 +1,5 @@
 # my node Project
 # npm i -D nodemon
+# npm i dotenv
+# npm i mongoose
+# npm i express
---
+++ src/index.js
@@ -0,0 +1,32 @@
+
+
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({path: ".env",});
+
+connectDB();
+
+
+
+// import express from "express";
+// const app = express();
+
+// ( async () => {
+//     try {
+//         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         console.log(`\n MongoDB connected successfully to host: ${connectionInstance.connection.host}`);
+//         app.on("error", (error) => {
+//             console.log("Server is running on port 8000");
+//             throw error;
+//         });
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`Server is running on port ${process.env.PORT}`);
+//         });
+//     } catch (error) {
+//         console.error("MongoDB connection error", error);
+//         throw error;
+//     }
+// })();
